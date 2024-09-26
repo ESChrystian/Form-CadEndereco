6b559f87f59d33635fa6d5a6f6849b207febd919
Chrystian de Almeida Silva <chrystian.almeida@alunoescolassociais.org.br>
2024-09-26 10:54:17 -0300

    Adiciona funções para limpar formulário e preencher formulário com os dados da api, por final expressões regulares de validação implementadas.

--- js/main.js
+++ js/main.js
@@ -15,5 +15,14 @@ const limparFormulario = ()=> {
 const eNumero = (numero) => /^[0-9] + $/.test(numero);
 
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);
-// length é ua propriedade que verifica a quantidade de caracteres do argumento cep
+// length é uma propriedade que verifica a quantidade de caracteres do argumento cep
+
+// Função de preenchimento de formulário com os dados de cep, buscado da API
+const preencherFormulario = (endereco) => {
+    document.getElementById('lougradouro').value = endereco.lougradouro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('Uf').value = endereco.Uf;
+    document.getElementById('bairro').value = endereco.bairro;
+}  
+
 
